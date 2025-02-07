0e1737d40e010f962f8498ea6992e3a90499f2cc
21Ravan12 <asgarovravan@gmail.com>
2025-02-07 12:12:23 +0400

    Update rooter.js

--- public/js/rooter.js
+++ public/js/rooter.js
@@ -9,26 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Sayfa başlangıcında ana sayfa görünsün
     navigateTo("main-page");
-
-    // Butonlara yönlendirme ekle
-    document.querySelector(".login-button").addEventListener("click", () => navigateTo("login-page"));
-    document.querySelector(".primary-cta-button").addEventListener("click", () => navigateTo("sign-up-page"));
-    
-    // Giriş ve kayıt sayfaları arasında geçiş
-    function MainToLogin() { navigateTo("login-page"); }
-    function MainToSignUp() { navigateTo("sign-up-page"); }
-    function LoginToSignin() { navigateTo("sign-up-page"); }
-    function SigninToLogin() { navigateTo("login-page"); }
-    function LoginToForgetpassword() { navigateTo("forget-password-page"); }
-    function ForgetpasswordToLogin() { navigateTo("login-page"); }
-    function LandingToAccount() { navigateTo("Landing-page"); }
     
     // HTML içinde onclick kullanıyorsan, bunları global yap
-    window.MainToLogin = MainToLogin;
-    window.MainToSignUp = MainToSignUp;
-    window.LoginToSignin = LoginToSignin;
-    window.SigninToLogin = SigninToLogin;
-    window.LoginToForgetpassword = LoginToForgetpassword;
-    window.ForgetpasswordToLogin = ForgetpasswordToLogin;
-    window.LandingToAccount = LandingToAccount;
+    window.navigateTo = navigateTo;
 });
