f9e4275a9668f73589466644dbf1a369da7ef663
21Ravan12 <asgarovravan@gmail.com>
2025-02-09 00:57:15 +0400

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,20 @@
-
+// Exporting configuration settings as a module
+module.exports = {
+    // MongoDB connection URI
+    mongoUri: 'mongodb://localhost:27017',
+    
+    // Name of the primary database
+    dbName: 'My_social_web_database',
+    
+    // Name of the secondary database
+    dbName1: 'chatApp',
+    
+    // Email credentials for nodemailer
+    emailCredentials: {
+      // Email address used for sending emails
+      user: '',
+      
+      // Password for the email account
+      pass: ''
+    }
+  };
